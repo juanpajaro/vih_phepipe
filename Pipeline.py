f5a64039c9cc33af5fb30040b0d0ba512f96d9a0
juanpajaro <pajaro@024EFME200273.JAVERIANA.LOC>
2025-03-26 09:29:15 -0500

    se modifico el modulo para que se ejecute primero concept extracction

--- Pipeline.py
+++ Pipeline.py
@@ -4,7 +4,7 @@ from dataset_transformation import DatasetTransformation
 from utils_class_split import DatasetSplit
 from conceptse import ClinicalExtraction
 
-path_data = "/home/pajaro/pipeline_project_v2/computational_pipe_v2/raw_data/"
+path_data = "./raw_data/"
 name_ehr_data = "Vista_Minable_3636.csv"
 name_poli_data = "fecha_cedula_clinica_suenio_may 31 2023.csv"
 name_sleepS_data = "base principal ajustada 11mayo2021.csv"
@@ -15,10 +15,9 @@ dataset_transformation = DatasetTransformation(path_data, name_poli_data, name_s
 dataset_transformation
 dataset_transformation.run_transformation_pipe()
 
-split = DatasetSplit("/early_data/early_prediction_data1.json")
-split
-split.run_pipe_ds()
+name_data = "/early_data/early_prediction_data1.json"
 
-extraction = ClinicalExtraction("/early_data/early_prediction_data1.json", "/map/map_icd10_umls.csv", "/destination_umls_es")
+extraction = ClinicalExtraction(name_data, "/map/map_icd10_umls.csv", "/destination_umls_es")
 extraction.run_pipe_ec()
 print("concept extraction ended...")
+
