31c3f93c91ad7e672e986fe526e83bd40323337f
juanpajaro <pajaro@024EFME200273.JAVERIANA.LOC>
2025-04-01 16:18:47 -0500

    se agrego la ruta adecuada al archivo para que corra

--- data_transformation_pipeline.py
+++ data_transformation_pipeline.py
@@ -137,7 +137,7 @@ def main(path_data, name_poli_data, name_sleepS_data, name_idcc, name_ehr_data, 
 
 # Ejecución del pipeline
 if __name__ == "__main__":
-    path_data = "/zine/data/salud/computational_pipe_v2/raw_data"
+    path_data = "/zine/data/salud/computational_pipe_v2/raw_data/"
     name_poli_data = "fecha_cedula_clinica_suenio_may 31 2023.csv"
     name_sleepS_data = "base principal ajustada 11mayo2021.csv"
     name_idcc = "3636_idClientes.csv"
