bfa8cb08383aaecd7d654257f2c9a625cce1aea8
juanpajaro <pajaro@024EFME200273.JAVERIANA.LOC>
2025-04-08 11:07:22 -0500

    agregado el paso de split_data_pipeline.py

---
+++ run_split.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# #SBATCH --job-name=split
+# #SBATCH --output=logs/split_%j.out
+# #SBATCH --error=logs/split_%j.err
+# #SBATCH --ntasks=1
+# #SBATCH --cpus-per-task=8
+# #SBATCH --mem=32G
+# #SBATCH --time=24:00:00
+
+source /zine/apps/anaconda_salud/etc/profile.d/conda.sh
+conda activate 1cphe
+echo "El ambiente activado es: "$CONDA_DEFAULT_ENV
+
+echo "Starting split job..."
+
+CURRENT_DATE=$(date +"%Y%m%d_%H%M%S")
+echo "Current date: $CURRENT_DATE"
+
+# Define paths
+PATH="/zine/data/salud/compu_Pipe_V3/"
+FILENAME="concepts/concepts_${CURREN_DATE}.json/"
+TRAIN_SIZE=0.8
+
+#run the pipeline
+srun python3 split_data.py $PATH $FILENAME $TRAIN_SIZE "$CURRENT_DATE"
---
+++ split_data_pipeline.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+import os
+import utils_general_porpose
+import utils_split_dataset
+import sys
+
+#Load data
+def load_data(path, filename):
+    data = utils_general_porpose.load_json(path, filename)
+    print("Data loaded")
+    return data
+
+def balanced_subsample(data):
+    balanced_data = utils_split_dataset.balanced_subsample(data)
+    print("Balanced data created")
+    return balanced_data
+
+def split_data(data, train_size):
+    train_data, test_data = utils_split_dataset.split_data(data, train_size)
+    print("Data split into train and test sets")
+    return train_data, test_data
+
+def save_data(data, directory, filename_prefix, timestamp):
+    json_path = os.path.join(directory, f"{filename_prefix}_{timestamp}.json")
+    utils_general_porpose.save_json(data, json_path)
+    print("Data saved")
+
+def main(path, filename, train_size, timestamp):
+    data = load_data(path, filename)
+    balanced_data = balanced_subsample(data)
+    train_data, test_data = split_data(balanced_data, train_size)
+    save_data(train_data, "./train", "train", timestamp)
+    save_data(test_data, "./test", "test", timestamp)
+    print("Split Data pipeline completed")
+
+if __name__ == "__main__":
+    if len(sys.argv) != 5:
+        print("Usage: python split_data_pipeline.py <path> <filename>")
+        sys.exit(1)
+    
+    path = sys.argv[1]    
+    filename = sys.argv[2]
+    train_size = sys.argv[3]
+    timestamp = sys.argv[4]
+
+
+    main(path, filename, train_size, timestamp)
