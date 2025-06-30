25a14896cd314972ee7214e83164b989a475d0ee
juan <juanpajaro@javeriana.edu.co>
2025-06-30 06:30:39 -0500

    probando el primer paso data preparation en el cluster

--- run_computational_phenotyping_pipeline.sh
+++ run_computational_phenotyping_pipeline.sh
@@ -23,19 +23,16 @@ echo "El ambiente activado es: "$CONDA_DEFAULT_ENV
 
 #RUN DATA TRANSFORMATION STEP
 # Define paths
-PATH_RAW_DATA="/zine/data/salud/computational_pipe_v2/raw_data/"
-#PATH_DATA="./raw_data/"
-NAME_POLI_DATA="fecha_cedula_clinica_suenio_may 31 2023.csv"
-NAME_SLEEP_DATA="base principal ajustada 11mayo2021.csv"
-NAME_IDCC="3636_idClientes.csv"
-NAME_EHR_DATA="Vista_Minable_3636.csv"
 DAYSPW=180
 DAYSOW=730
+PATH_RAW_DATA="/zine/data/salud/vih_phepipe/base_datos/"
+DATA_NAME="Variables_HC.txt"
+LABEL_NAME='Etiqueta.txt'
 
 #Create folder for logs if it doesn't exist
 mkdir -p logs
 
-srun python3 data_transformation.py "$PATH_RAW_DATA" "$NAME_POLI_DATA" "$NAME_SLEEP_DATA" "$NAME_IDCC" "$NAME_EHR_DATA" $DAYSPW $DAYSOW "$CURRENT_DATE"
+srun python3 wo_data_preparation.py $DAYSPW $DAYSOW "$CURRENT_DATE" "$PATH_RAW_DATA" "$DATA_NAME" "$LABEL_NAME" 
 
 # Define paths
 PATH_DATA_TRAIN="data_transformation/data_t_${CURRENT_DATE}.json"
@@ -58,7 +55,7 @@ LIST_AS_STRING=$(IFS=,; echo "${LISTA_CAT[*]}")
 #mkdir -p logs
 
 # Run the pipeline
-srun python3 clinical_concept_extraction.py $PATH_DATA_TRAIN $CURRENT_PATH $UMLS_TO_ICD_PATH $QUMLS_PATH $NUM_PROCESSES "$CURRENT_DATE" $SIMILARITY_THRESHOLD "$LIST_AS_STRING" "$DICTIONARY_ICD_LOCAL"
+#srun python3 clinical_concept_extraction.py $PATH_DATA_TRAIN $CURRENT_PATH $UMLS_TO_ICD_PATH $QUMLS_PATH $NUM_PROCESSES "$CURRENT_DATE" $SIMILARITY_THRESHOLD "$LIST_AS_STRING" "$DICTIONARY_ICD_LOCAL"
 #python3 clinical_concept_extraction_pipeline_v2.py $PATH_DATA_TRAIN $CURRENT_PATH $UMLS_TO_ICD_PATH $QUMLS_PATH $NUM_PROCESSES "$CURRENT_DATE" $SIMILARITY_THRESHOLD "$LIST_AS_STRING"
 
 #RUN DATA SPLITTING STEP
@@ -67,7 +64,7 @@ srun python3 clinical_concept_extraction.py $PATH_DATA_TRAIN $CURRENT_PATH $UMLS
 #PATH_DATA="/home/pajaro/compu_Pipe_V3/"
 FILENAME="/concepts/clinical_concepts_${CURRENT_DATE}.json"
 TRAIN_SIZE=0.8
-srun python3 split_data.py "$CURRENT_PATH" "$FILENAME" $TRAIN_SIZE "$CURRENT_DATE"
+#srun python3 split_data.py "$CURRENT_PATH" "$FILENAME" $TRAIN_SIZE "$CURRENT_DATE"
 #python3 split_data_pipeline.py "$PATH_DATA" "$FILENAME" $TRAIN_SIZE "$CURRENT_DATE"
 
 #RUN LSTM TRAINING STEP
@@ -85,11 +82,11 @@ MAX_LEN=$DAYSOW
 # Run the model
 #p_report=$(python3 train_lstm.py "$CURRENT_DATE" "$PATH_DATA" $MAX_TOKEN $MAX_LEN)
 #p_report=$(srun python3 train_lstm.py "$CURRENT_DATE" "$PATH_DATA" $MAX_TOKEN $MAX_LEN)
-srun python3 train_lstm_loop.py "$CURRENT_DATE" "$CURRENT_PATH" $MAX_TOKEN $MAX_LEN "$LIST_AS_STRING" "$DICTIONARY_ICD_LOCAL"
+#srun python3 train_lstm_loop.py "$CURRENT_DATE" "$CURRENT_PATH" $MAX_TOKEN $MAX_LEN "$LIST_AS_STRING" "$DICTIONARY_ICD_LOCAL"
 #python3 train_lstm_loop.py "$CURRENT_DATE" "$PATH_DATA" $MAX_TOKEN $MAX_LEN
 
 # Run the attention model
-srun python3 train_attention.py "$CURRENT_DATE" "$CURRENT_PATH" $MAX_TOKEN "$LIST_AS_STRING" "$DICTIONARY_ICD_LOCAL"
+#srun python3 train_attention.py "$CURRENT_DATE" "$CURRENT_PATH" $MAX_TOKEN "$LIST_AS_STRING" "$DICTIONARY_ICD_LOCAL"
 
 #Run logistic regression model
-srun python3 train_logistic.py "$CURRENT_DATE" "$CURRENT_PATH" $MAX_LEN "$LIST_AS_STRING" "$DICTIONARY_ICD_LOCAL"
+#srun python3 train_logistic.py "$CURRENT_DATE" "$CURRENT_PATH" $MAX_LEN "$LIST_AS_STRING" "$DICTIONARY_ICD_LOCAL"
